61d1568be6af3610efa5a9dbaa60718586c311eb
AbdelrhmanAmer <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>
2023-12-18 15:52:46 +0200

    Fluttertoast

--- flutter_ui/lib/main.dart
+++ flutter_ui/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() {
   runApp(const MyApp());
@@ -27,19 +28,69 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-Widget buildText() {
-  return const Text('Flutter Demo Home Page');
+Widget buildText(BuildContext context,{required Color color, required String title, required double font}) {
+  return Text(title, style: Theme.of(context).textTheme.titleMedium!.copyWith(
+    color: color,
+    fontSize: font,
+    fontWeight: FontWeight.bold,
+  ),
+  );
 }
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    final Color bodyColor = Theme.of(context).colorScheme.onBackground;
+    final Color appBarColor = Theme.of(context).colorScheme.primary;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: buildText(),
+        title: buildText( context, color: appBarColor, title: 'Toast Demo', font: 25),
       ),
-      body: null,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            ElevatedButton(
+                onPressed: (){
+                  Fluttertoast.showToast(
+                      msg: "This is Center Short Toast",
+                      toastLength: Toast.LENGTH_SHORT,
+                      gravity: ToastGravity.CENTER,
+                      timeInSecForIosWeb: 1,
+                      backgroundColor: Colors.red,
+                      textColor: Colors.white,
+                      fontSize: 16.0
+                  );
+                },
+                child: buildText(context,color: bodyColor, title: 'Red Center Short Toast', font: 20),
+            ),
+            ElevatedButton(
+              onPressed: (){
+                Fluttertoast.showToast(
+                    msg: "This is Buttom Short Toast",
+                    toastLength: Toast.LENGTH_SHORT,
+                    gravity: ToastGravity.BOTTOM,
+                    timeInSecForIosWeb: 1,
+                    backgroundColor: Colors.green,
+                    textColor: Colors.white,
+                    fontSize: 16.0,
+
+                );
+              },
+              child: buildText(context,color: bodyColor, title: 'Green Buttom Short Toast', font: 20),
+            ),
+
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
     );
   }
-
 }
